e9f9a6f3ea2985d9418c7b09b36473291d119791
Ian Miller <imiller@redhat.com>
2024-10-04 10:48:27 -0400

    Updating yamllint installer for ubi env
    Signed-off-by: Ian Miller <imiller@redhat.com>

--- hack/install-lint.sh
+++ hack/install-lint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -xe
 
-yum -y install yamllint
+command -v microdnf
+if [[ $? -eq 0 ]] ; then
+    microdnf install -y python3 python3-pip
+    python3 -m pip install yamllint
+else
+    echo "Unexpected environment, not installing yamllint"
+fi
+
