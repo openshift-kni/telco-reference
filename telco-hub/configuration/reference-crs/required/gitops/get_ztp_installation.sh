23e66d2407e5e9928dfd5150633a92946f3c6895
Jose Gato <jgato@redhat.com>
2024-11-05 11:06:01 +0100

    Folder to keep manifests to install ZTP
    Included an script that generates the folder from
    the corresponding ztp-site-generate image. The
    idea is to use this script, only once, per each
    OCP/Hub version.
    
    Currently contains the manifestes from 4.16. In
    any moment, we can check from which image these
    were extracted. Just checking the file
    `manifests.ver`.

---
+++ telco-hub/configuration/reference-crs/required/gitops/get_ztp_installation.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# This script is only needed to create the ztp-installation manifest
+# once per each Minor version.
+
+CURRENT_OCP_VERSION=4.16
+ZTP_SITE_GENERATE_IMAGE=ztp-site-generate-rhel8:v${CURRENT_OCP_VERSION}
+podman run --log-driver=none --rm registry.redhat.io/openshift4/${ZTP_SITE_GENERATE_IMAGE=ztp-site-generate-rhel8} extract /home/ztp/argocd/deployment --tar | tar x -C "ztp-installation"
+echo "Manifests created from ${ZTP_SITE_GENERATE_IMAGE}" > ztp-installation/manifests.ver
