569bf1f3310c5b3f61e4cfea694385a939bb3641
Jose Gato <jgato@redhat.com>
2024-11-15 12:10:25 +0100

    updated ztp installation manifests to 4.17

--- telco-hub/configuration/reference-crs/required/gitops/get_ztp_installation.sh
+++ telco-hub/configuration/reference-crs/required/gitops/get_ztp_installation.sh
@@ -3,7 +3,7 @@
 # This script is only needed to create the ztp-installation manifest
 # once per each Minor version.
 
-CURRENT_OCP_VERSION=4.16
+CURRENT_OCP_VERSION=4.17
 ZTP_SITE_GENERATE_IMAGE=ztp-site-generate-rhel8:v${CURRENT_OCP_VERSION}
 podman run --log-driver=none --rm registry.redhat.io/openshift4/${ZTP_SITE_GENERATE_IMAGE=ztp-site-generate-rhel8} extract /home/ztp/argocd/deployment --tar | tar x -C "ztp-installation"
 echo "Manifests created from ${ZTP_SITE_GENERATE_IMAGE}" > ztp-installation/manifests.ver
