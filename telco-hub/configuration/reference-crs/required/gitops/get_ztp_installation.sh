a769c590f458b423fd630817a394573275593333
Jose Gato <jgato@redhat.com>
2025-04-29 17:25:12 +0200

    recovering missing sync-waves
    and other missed configurations

--- telco-hub/configuration/reference-crs/required/gitops/get_ztp_installation.sh
+++ telco-hub/configuration/reference-crs/required/gitops/get_ztp_installation.sh
@@ -6,9 +6,11 @@
 #CURRENT_OCP_VERSION=4.18
 #ZTP_SITE_GENERATE_IMAGE=ztp-site-generate-rhel8:v${CURRENT_OCP_VERSION}
 #podman run --log-driver=none --rm registry.redhat.io/openshift4/${ZTP_SITE_GENERATE_IMAGE=ztp-site-generate-rhel8} extract /home/ztp/argocd/deployment --tar | tar x -C "ztp-installation"
-# some generated manifests are not needed
-#rm ztp-installation/allow-acm-managedcluster-control.json ztp-installation/disable-cluster-proxy-addon.json ztp-installation/openshift-gitops-operator.yaml
 
 rm ztp-installation/*
 cp ../../../../../telco-ran/configuration/argocd/deployment/* ztp-installation/
 echo "Manifests created from 'telco-ran/configuration/argocd/deployment/'"
+# some generated manifests are not needed
+echo " - Removing some manifests not needed: allow-acm-managedcluster-control.json, disable-cluster-proxy-addon.json, openshift-gitops-operator.yaml"
+rm ztp-installation/allow-acm-managedcluster-control.json ztp-installation/disable-cluster-proxy-addon.json ztp-installation/openshift-gitops-operator.yaml
+
