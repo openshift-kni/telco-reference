c9ee37402269bb094b642677e4d87c87a14dc9df
Jose Gato <jgato@redhat.com>
2025-06-16 15:18:04 +0200

    gitops to match RDS docs
    missing many manifests about mangaing gitops
    from argocd

--- telco-hub/configuration/reference-crs/required/gitops/get_ztp_installation.sh
+++ telco-hub/configuration/reference-crs/required/gitops/get_ztp_installation.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
-set -euo pipefail
 
 # This script is only needed to create the ztp-installation manifest
 # once per each Minor version.
 
+#CURRENT_OCP_VERSION=4.18
+#ZTP_SITE_GENERATE_IMAGE=ztp-site-generate-rhel8:v${CURRENT_OCP_VERSION}
+#podman run --log-driver=none --rm registry.redhat.io/openshift4/${ZTP_SITE_GENERATE_IMAGE=ztp-site-generate-rhel8} extract /home/ztp/argocd/deployment --tar | tar x -C "ztp-installation"
+
 rm ztp-installation/*
-
-# we create the manifests from the 'telco-ran/configuration/argocd/deployment/'"
-
 cp ../../../../../telco-ran/configuration/argocd/deployment/* ztp-installation/
 echo "Manifests created from 'telco-ran/configuration/argocd/deployment/'"
-
 # some generated manifests are not needed
 echo " - Removing some manifests not needed: allow-acm-managedcluster-control.json, disable-cluster-proxy-addon.json, openshift-gitops-operator.yaml"
 rm ztp-installation/allow-acm-managedcluster-control.json ztp-installation/disable-cluster-proxy-addon.json ztp-installation/openshift-gitops-operator.yaml
